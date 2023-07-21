e7b863eede554087ee35911ef46dcc5c29e9b4f1
Bui <vietbui20@augustana.edu>
2023-07-21 16:05:13 -0500

    Solved concurrent issue with manual operation and automatic opertation, may need further trouble shooting after the motorized reel is built

--- analogTestwAccelStepper.ino
+++ analogTestwAccelStepper.ino
@@ -16,47 +16,59 @@ int analogPin = A0;
 int cwPin = A1;
 int ccwPin = A2;
 int DOUT = A3;
-const int rev = 800;
+const int rev = 800*2;
 int count = 1;
 
-bool boo;
+bool boo = false;       // for XLink
+bool booManual = false; // for manual control 
 
 void setup() {
-  motor.setMaxSpeed(1000);
-  motor.setAcceleration(500);
+  motor.setMaxSpeed(500);
+  motor.setAcceleration(300);
 }
 
 void loop() {
   int cw = analogRead(cwPin);
   int ccw = analogRead(ccwPin);
-  int xLink = analogRead(DOUT);
-  /*Serial.print("CW");
-  Serial.println(cw);
-  Serial.print("CCW");
-  Serial.println(ccw);*/
-//  Serial.println(xLink);
-  if (cw > 700) {
+  int xLink = analogRead(DOUT); 
+  int val = analogRead(analogPin);
+  
+  if (cw > 700) { // >700 because max voltage is 3V
     digitalWrite(dirPin, HIGH);
   }
   if (ccw > 700) {
     digitalWrite(dirPin, LOW);
   }
   
-  int val = analogRead(analogPin);
-  if ((val > 1000) || (xLink > 1000)) {
+  // XLink send a 1s 5V from the DOUT port to the analog port. 
+  if (xLink > 1000) { // >1000 for 5V signal
     boo = true;
   } else {
     boo = false;
   }
-  //Serial.println(boo);
-  if ((boo) && (count <= 5)) {
-    int currentRev = rev * count;
-    motor.moveTo(currentRev);
-    motor.runToPosition();
-    count = count + 1;
-  } else if ((boo) && (count > 5)) {
-    motor.moveTo(0);
-    motor.runToPosition();
-    count = 1;
+
+  // Manual button engage overwrite the xLink boolean value
+  if (val > 1000) {
+    boo = false;
+    booManual = true;
+  } else {
+    booManual = false;
+  }
+  if (booManual) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(1000);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(1000);
+  } else {
+    if ((boo) && (count <= 5)) {
+      int currentRev = rev * count;
+      motor.moveTo(currentRev);
+      motor.runToPosition();
+      count = count + 1;
+    } else if ((boo) && (count > 5)) {
+      motor.moveTo(0);
+      motor.runToPosition();
+      count = 1;
+    }
   }
 }
